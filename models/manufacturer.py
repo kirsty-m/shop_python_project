a04a50acfb9b20b632bb770136ca898e3f0fb92b
Kirsty <kirstmcbride@gmail.com>
2023-06-10 09:52:25 +0100

    classes added

---
+++ models/manufacturer.py
@@ -0,0 +1,5 @@
+class Manufacturer:
+    def __init__(self, name, location, id = None):
+        self.name = name
+        self.location = location
+        self.id = id
---
+++ models/product.py
@@ -0,0 +1,10 @@
+class Product:
+    def __init__(self, name, description, stock_quantity, buying_cost, selling_price, category, id = None, manufacturer):
+        self.name = name
+        self.description = description
+        self.stock_quantity = stock_quantity
+        self.buying_cost = buying_cost
+        self.selling_price = selling_price
+        self.category = category
+        self.id = id
+        self.manufacturer = manufacturer
